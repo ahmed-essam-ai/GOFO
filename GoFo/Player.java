fd131554d5e12bff8a34686111d2dbd9fe6a34aa
elhusseinn <elhusseinabdalaziz1@gmail.com>
2021-06-12 16:05:33 +0200

    added part of java doc.

--- GoFo/Player.java
+++ GoFo/Player.java
@@ -1,5 +1,10 @@
 package GoFo;
-
+/**
+ * Class Main used to display meny and taking data from user.
+ *  @author Ahmed Essam El din 20190050 , Habiba Tarek Fathi 20190168 and Al-Hossain Abdalaziz Youssef 20190106
+ * @version 1.3
+ * @since 2020-6-10
+ */
 public class Player extends User {
 
 
