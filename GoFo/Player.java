5d24e9adb6bf1913db9c060fbf70e698563fc7d8
elhusseinn <elhusseinabdalaziz1@gmail.com>
2021-06-12 16:17:40 +0200

    documentation update to player class.

--- GoFo/Player.java
+++ GoFo/Player.java
@@ -7,7 +7,14 @@ package GoFo;
  */
 public class Player extends User {
 
-
+    /**
+     * Player parametrized constructor that takes owner data.
+     * @param name      the second parameter to player parametrized constructor of type String that hold the name of the player.
+     * @param password  the third parameter to player parametrized constructor of type String that hold the password of the player.
+     * @param Email       the fourth parameter to player parametrized constructor of type String that hold the email of the player.
+     * @param phoneNumber the fifth parameter to player parametrized constructor of type String to hold the phone of the player.
+     * @param address   the sixth parameter to player parametrized constructor of type String to hold the address of the player.
+     */
     public Player(String name, String password, String Email, String phoneNumber, String address){
         super(name, password, Email, phoneNumber, address);
         setType("player");
