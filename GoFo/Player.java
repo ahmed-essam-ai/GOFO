7db161acc1c0ac1c8a5b6a4d7037da9d7eecc737
habiba157 <77742360+habiba157@users.noreply.github.com>
2021-06-12 21:46:44 +0200

    Update Player.java

--- GoFo/Player.java
+++ GoFo/Player.java
@@ -1,6 +1,6 @@
 package GoFo;
 /**
- * Class Main used to display meny and taking data from user.
+ * Class Main used to display menu and taking data from user.
  *  @author Ahmed Essam El din 20190050 , Habiba Tarek Fathi 20190168 and Al-Hossain Abdalaziz Youssef 20190106
  * @version 1.3
  * @since 2020-6-10
